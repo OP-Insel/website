9c23cb8070798749bc8088d64c01381155e69bbf
OP-Insel <benetitomas63@gmail.com>
2025-03-07 19:09:01 +0100

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,16 @@
+export interface User {
+  id: string
+  name: string
+  minecraftName: string
+  rank: string
+  points: number
+}
+
+export interface Event {
+  id: string
+  title: string
+  description: string
+  date: string
+  time: string
+  type: "meeting" | "deadline"
+}
